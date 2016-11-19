044a7bb094d3a2e1f8d4e76d66597532ac7e86e1
Robert Deans <robdeans@gmail.com>
2016-11-19 16:30:40 -0500

    added initializers

--- PupsAndPlaygrounds/PupsAndPlaygrounds/Locations.swift
+++ PupsAndPlaygrounds/PupsAndPlaygrounds/Locations.swift
@@ -10,10 +10,23 @@ import Foundation
 
 struct Playground {
     
-    
+    let playgroundID: String
+    let name: String
+    let location: String
+    var isHandicap: Bool = false
+    let latitude: Double
+    let longitude: Double
     
 }
 
 struct Dogruns {
+    
+    let playgroundID: String
+    let name: String
+    let location: String
+    let dogRunType: String
+    let notes: String
+    var isHandicap: Bool = false
+    
     
 }
