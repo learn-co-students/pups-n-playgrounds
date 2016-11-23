d006abf02fb56640d2e14a6c6b63f9733990126c
Robert Deans <robdeans@gmail.com>
2016-11-23 15:29:02 -0500

    updates views

--- PupsAndPlaygrounds/PupsAndPlaygrounds/LocationsProfileView.swift
+++ PupsAndPlaygrounds/PupsAndPlaygrounds/LocationsProfileView.swift
@@ -11,36 +11,48 @@ import SnapKit
 
 class LocationsProfileView: UIView {
     
-    var nameField: UILabel!
-    var locationField: UILabel!
-    var ratingField: UILabel!
+    var nameLabel: UILabel!
+    var locationLabel: UILabel!
+    var handicapLabel: UILabel!
+    //  var ratingLabel: UILabel!
     //  var reviewsTableView: UITableView!
+    var reviewTextField: UITextField!
+    var ratingTextField: UITextField!
     var addReviewButton: UIButton!
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-    
+    // inevitably the argument will be from Firebase and not the local LocationsDataStore
     convenience init(location: Location) {
         self.init(frame: CGRect.zero)
         configure(location: location)
         constrain()
     }
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
     required init?(coder aDecoder: NSCoder) {
-        
         super.init(coder: aDecoder)
     }
     
     func configure(location: Location) {
         backgroundColor = UIColor.themeLightBlue
-
-        nameField = UILabel()
-        nameField.text = location.name
-        locationField.text = location.location
-
+        
+        nameLabel = UILabel()
+        locationLabel = UILabel()
+        handicapLabel = UILabel()
+        
+        nameLabel.text = location.name
+        locationLabel.text = location.location
+        if location.isHandicap == true {
+            handicapLabel.text = "Is accessible"
+        }else {
+            self.handicapLabel.text = "Is not accessible"
+        }
+        
+        
         
     }
+    
     
     func constrain() {
         
