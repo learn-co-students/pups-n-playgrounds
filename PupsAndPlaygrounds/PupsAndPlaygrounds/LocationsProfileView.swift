1eeb690a23a2baa08c653f030ce674a17f1c8b0c
Robert Deans <robdeans@gmail.com>
2016-11-23 13:50:59 -0500

    adds locations vc

---
+++ PupsAndPlaygrounds/PupsAndPlaygrounds/LocationsProfileView.swift
@@ -0,0 +1,9 @@
+//
+//  LocationsProfileView.swift
+//  PupsAndPlaygrounds
+//
+//  Created by Robert Deans on 11/23/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
