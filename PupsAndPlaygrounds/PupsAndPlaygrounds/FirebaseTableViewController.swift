d28f0c23b03946ad12736d8856a96df33042cc3a
wsrobins <wsrobins@uvm.edu>
2016-11-29 10:56:05 -0500

    added toggle to home vc

--- PupsAndPlaygrounds/PupsAndPlaygrounds/FirebaseTableViewController.swift
+++ PupsAndPlaygrounds/PupsAndPlaygrounds/FirebaseTableViewController.swift
@@ -11,26 +11,31 @@ import UIKit
 class FirebaseTableViewController: UITableViewController {
   
   var playgroundArray: [Playground] = []
+  var isMapView = false
   let appDelegate = UIApplication.shared.delegate as? AppDelegate
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    title = "Playgrounds & Dog Runs"
+    title = "List View"
+    navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "Switch View"), style: .plain, target: self, action: #selector(switchView))
     
     tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Playground")
     FirebaseData.getAllPlaygrounds { (playgroundsFromFirebase) in
       self.playgroundArray = playgroundsFromFirebase
       self.tableView.reloadData()
     }
-
+  }
+  
+  func switchView() {
+    title = isMapView ? "List View" : "Map View"
+    isMapView = !isMapView
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     let playgroundCount = playgroundArray.count
     return playgroundCount
   }
-  
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "Playground", for: indexPath)
