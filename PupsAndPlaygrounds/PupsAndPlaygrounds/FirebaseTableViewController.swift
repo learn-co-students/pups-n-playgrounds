2f97316717b845eb62262710317a55b805b8356f
wsrobins <wsrobins@uvm.edu>
2016-11-29 10:33:07 -0500

    edit vc titles

--- PupsAndPlaygrounds/PupsAndPlaygrounds/FirebaseTableViewController.swift
+++ PupsAndPlaygrounds/PupsAndPlaygrounds/FirebaseTableViewController.swift
@@ -9,48 +9,45 @@
 import UIKit
 
 class FirebaseTableViewController: UITableViewController {
+  
+  var playgroundArray: [Playground] = []
+  let appDelegate = UIApplication.shared.delegate as? AppDelegate
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    var playgroundArray: [Playground] = []
-    let appDelegate = UIApplication.shared.delegate as? AppDelegate
+    title = "Playgrounds & Dog Runs"
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        navigationItem.title = "Playgrounds & Dog Runs"
-        
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Playground")
-        FirebaseData.getAllPlaygrounds { (playgroundsFromFirebase) in
-            self.playgroundArray = playgroundsFromFirebase
-            self.tableView.reloadData()
-        }
-        
-        
-        
-        
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let playgroundCount = playgroundArray.count
-        return playgroundCount
-    }
-    
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Playground", for: indexPath)
-        
-        let playground = playgroundArray[indexPath.row]
-        
-        cell.textLabel?.text = playground.name
-        
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let locationProfileVC = LocationProfileViewController()
-        locationProfileVC.playground = playgroundArray[indexPath.row]
-        
-        navigationController?.pushViewController(locationProfileVC, animated: true)
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Playground")
+    FirebaseData.getAllPlaygrounds { (playgroundsFromFirebase) in
+      self.playgroundArray = playgroundsFromFirebase
+      self.tableView.reloadData()
     }
 
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    let playgroundCount = playgroundArray.count
+    return playgroundCount
+  }
+  
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Playground", for: indexPath)
     
+    let playground = playgroundArray[indexPath.row]
+    
+    cell.textLabel?.text = playground.name
+    
+    return cell
+  }
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let locationProfileVC = LocationProfileViewController()
+    locationProfileVC.playground = playgroundArray[indexPath.row]
+    
+    navigationController?.pushViewController(locationProfileVC, animated: true)
+  }
+  
+  
 }
