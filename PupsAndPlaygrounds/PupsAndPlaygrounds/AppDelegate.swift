3c7d2dbe959e94f99159d946bfcd727372b88975
wsrobins <wsrobins@uvm.edu>
2016-11-29 13:34:44 -0500

    ensure functional toggle

--- PupsAndPlaygrounds/PupsAndPlaygrounds/AppDelegate.swift
+++ PupsAndPlaygrounds/PupsAndPlaygrounds/AppDelegate.swift
@@ -38,6 +38,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   }
   
   func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+    
+    
+    
     return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
   }
   
