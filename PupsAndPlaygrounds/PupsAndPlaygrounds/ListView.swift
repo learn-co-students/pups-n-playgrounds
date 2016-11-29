0b79c477e003dfa6ab3279b0a7d37ca92955e8f2
wsrobins <wsrobins@uvm.edu>
2016-11-29 12:35:52 -0500

    finish map/list toggle functionality

--- PupsAndPlaygrounds/PupsAndPlaygrounds/ListView.swift
+++ PupsAndPlaygrounds/PupsAndPlaygrounds/ListView.swift
@@ -1,5 +1,5 @@
 //
-//  MapView.swift
+//  ListView.swift
 //  PupsAndPlaygrounds
 //
 //  Created by William Robinson on 11/29/16.
@@ -7,13 +7,12 @@
 //
 
 import UIKit
-import MapKit
 import SnapKit
 
-class MapView: UIView {
+class ListView: UIView {
   
   // MARK: Properties
-  let map = MKMapView()
+  let locationsTableView = UITableView()
   
   // MARK: Initialization
   required init?(coder aDecoder: NSCoder) {
@@ -38,8 +37,8 @@ class MapView: UIView {
   
   // MARK: View Constraints
   func constrain() {
-    addSubview(map)
-    map.snp.makeConstraints {
+    addSubview(locationsTableView)
+    locationsTableView.snp.makeConstraints {
       $0.edges.equalToSuperview()
     }
   }
