1922919d8257dfc80bce325fd4f6a51236bb7183
wsrobins <wsrobins@uvm.edu>
2016-11-30 16:41:02 -0500

    pod issues

--- PupsAndPlaygrounds/LocationViewPlayground.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-import XCPlayground
-
-
-var currentPage = LocationProfileViewController()
-
-XCPlaygroundPage.currentPage.liveView = currentPage
