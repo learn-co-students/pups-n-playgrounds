f5846919f99cd372589c2f544c4b85493d836ea3
fjohnson1991 <felicity.johnson.mail@gmail.com>
2016-12-02 13:11:08 -0500

    fixed conflicts

--- PupsAndPlaygrounds/PupsAndPlaygrounds/FeedViewController.swift
+++ PupsAndPlaygrounds/PupsAndPlaygrounds/FeedViewController.swift
@@ -14,96 +14,49 @@ class FeedViewController: UIViewController {
     // MARK: Properties
     let listView = ListView()
     var locations = [Location]()
-    
-<<<<<<< HEAD
-    feedView.feedTableView.delegate = self
-    feedView.feedTableView.dataSource = self
-    feedView.feedTableView.register(FeedTableViewCell.self, forCellReuseIdentifier: "feedCell")
-    
-    view.addSubview(feedView)
-    feedView.snp.makeConstraints {
-      $0.edges.equalToSuperview()
-    }
-    
-    
-  }
+    var feedView = FeedView()
     
     
     func flagButtonTouched() {
         
         print("button touched")
     }
-  
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destinationViewController.
-   // Pass the selected object to the new view controller.
-   }
-   */
-  
-=======
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        FirebaseData.getAllPlaygrounds { playgrounds in
-            self.locations = playgrounds
-            self.listView.locationsTableView.reloadData()
-        }
         
-        title = "Temporary Work Window!"
+        feedView.feedTableView.delegate = self
+        feedView.feedTableView.dataSource = self
+        feedView.feedTableView.register(FeedTableViewCell.self, forCellReuseIdentifier: "feedCell")
         
-        listView.locationsTableView.delegate = self
-        listView.locationsTableView.dataSource = self
-        listView.locationsTableView.register(UITableViewCell.self, forCellReuseIdentifier: "locationCell")
-        
-        view.addSubview(listView)
-        listView.snp.makeConstraints {
+        view.addSubview(feedView)
+        feedView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
         
     }
-    
->>>>>>> reviews-rdfj
 }
 
 // MARK: - UITableViewDelegate
+
 extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
-<<<<<<< HEAD
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "feedCell", for: indexPath) as! FeedTableViewCell
     
-    cell.titleLabel.text = "Feed Post \(indexPath.row + 1)"
-    
-    cell.flagButton.addTarget(self, action: #selector(flagButtonTouched), for: .touchUpInside)
-
-
-    return cell
-  }
-=======
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return locations.count
+        return 10
     }
     
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "locationCell", for: indexPath)
-        cell.textLabel?.text = locations[indexPath.row].name
+        let cell = tableView.dequeueReusableCell(withIdentifier: "feedCell", for: indexPath) as! FeedTableViewCell
+        
+        cell.titleLabel.text = "Feed Post \(indexPath.row + 1)"
+        
+        cell.flagButton.addTarget(self, action: #selector(flagButtonTouched), for: .touchUpInside)
+        
         
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let locationProfileVC = LocationProfileViewController()
-        guard let playground = locations[indexPath.row] as? Playground else { print("error downcasting to playground"); return }
-        
-        locationProfileVC.playground = playground
-        navigationController?.pushViewController(locationProfileVC, animated: true)
-    }
->>>>>>> reviews-rdfj
 }
