491d65a7eef5728d6b0b90c89f76155692d0cce1
Robert Deans <robdeans@gmail.com>
2016-12-08 17:53:05 -0500

    constraints work :D

--- PupsAndPlaygrounds/MinimalistContraintsPlayground.playground/Contents.swift
+++ PupsAndPlaygrounds/MinimalistContraintsPlayground.playground/Contents.swift
@@ -7,9 +7,14 @@ import XCPlayground
 
 var str = "Hello, playground"
 
+
+
 class LocationView: UIView {
     var scrollView: UIScrollView!
     var streetView: UIView!
+    var locationProfileImage: UIView!
+    var locationNameLabel: UILabel!
+    var locationAddressLabel: UILabel!
 
     
     func configure() {
@@ -21,6 +26,22 @@ class LocationView: UIView {
         streetView = UIView()
         streetView.backgroundColor = UIColor.cyan
 
+        locationProfileImage = UIView()
+        locationProfileImage.backgroundColor = UIColor.magenta
+        locationProfileImage.layer.cornerRadius = 10
+        
+        locationNameLabel = UILabel()
+        locationNameLabel.text = "American Playground"
+        locationNameLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 24)
+        locationNameLabel.lineBreakMode = NSLineBreakMode.byWordWrapping
+        locationNameLabel.numberOfLines = 2
+
+        locationAddressLabel = UILabel()
+        locationAddressLabel.text = "123 Main Street btw Dog and Child"
+        locationAddressLabel.font = UIFont(name: "HelveticaNeue", size: 16)
+        locationAddressLabel.numberOfLines = 0
+        locationAddressLabel.lineBreakMode = NSLineBreakMode.byWordWrapping
+        
         
     }
     
@@ -31,12 +52,39 @@ class LocationView: UIView {
         }
         
         
-        addSubview(streetView)
+        scrollView.addSubview(streetView)
         streetView.snp.makeConstraints {
-            $0.top.leading.trailing.equalTo(scrollView)
-            $0.height.equalTo(streetView.snp.width)
-            
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(scrollView.snp.top)
+            $0.width.equalTo(scrollView.snp.width)
+            $0.height.equalTo(streetView.snp.width).multipliedBy(0.6)
         }
+        
+        scrollView.addSubview(locationProfileImage)
+        locationProfileImage.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(10)
+            $0.top.equalTo(streetView.snp.bottom).offset(10)
+            $0.width.equalToSuperview().dividedBy(4)
+            $0.height.equalTo(locationProfileImage.snp.width)
+        }
+        
+        scrollView.addSubview(locationNameLabel)
+        locationNameLabel.snp.makeConstraints {
+            $0.leading.equalTo(locationProfileImage.snp.trailing).offset(10)
+            $0.trailing.equalTo(scrollView.snp.trailing)
+            $0.top.equalTo(streetView.snp.bottom).offset(12)
+            $0.height.equalTo(locationProfileImage).dividedBy(2)
+            $0.width.equalToSuperview().multipliedBy(0.66)
+        }
+        
+//        scrollView.addSubview(locationAddressLabel)
+//        locationAddressLabel.snp.makeConstraints {
+//            $0.leading.equalTo(locationProfileImage.snp.trailing).offset(5)
+//            $0.trailing.equalToSuperview()
+//            $0.top.equalTo(locationNameLabel.snp.bottom).offset(10)
+//            $0.height.equalTo(locationProfileImage).multipliedBy(0.66)
+//        }
+        
         
     }
 
--- PupsAndPlaygrounds/PupsAndPlaygrounds/LocationsProfileView.swift
+++ PupsAndPlaygrounds/PupsAndPlaygrounds/LocationsProfileView.swift
@@ -24,6 +24,7 @@ class LocationProfileView: UIView, GMSMapViewDelegate {
     var starReviews: StarReview!
     var rating: String?
     var scrollView: UIScrollView!
+    var contentView: UIView!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -57,9 +58,12 @@ class LocationProfileView: UIView, GMSMapViewDelegate {
             self.starReviews.starFillColor = UIColor.red
             self.starReviews.starBackgroundColor = UIColor.black
             self.starReviews.starMarginScale = 0.3
+            self.starReviews.contentMode = .scaleAspectFit
         }
         scrollView = UIScrollView()
         scrollView.contentSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 1.5)
+        
+        contentView = UIView()
         
         backgroundColor = UIColor.themeLightBlue
         
@@ -69,7 +73,6 @@ class LocationProfileView: UIView, GMSMapViewDelegate {
         locationProfileImage.clipsToBounds = true
         
         streetView = UIView()
-        streetView.backgroundColor = UIColor.cyan
         panoView = GMSPanoramaView()
         panoView.moveNearCoordinate(CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
         panoView.layer.cornerRadius = 5
@@ -79,14 +82,14 @@ class LocationProfileView: UIView, GMSMapViewDelegate {
         locationNameLabel.textColor = UIColor.themeDarkBlue
         locationNameLabel.text = location.name
         locationNameLabel.adjustsFontSizeToFitWidth = true
-        locationNameLabel.numberOfLines = 2
+        locationNameLabel.numberOfLines = 0
         locationNameLabel.lineBreakMode = NSLineBreakMode.byWordWrapping
         
         locationAddressLabel = UILabel()
         locationAddressLabel.font = UIFont.themeSmallRegular
         locationAddressLabel.textColor = UIColor.themeDarkBlue
         locationAddressLabel.text = location.address
-        locationAddressLabel.numberOfLines = 3
+        locationAddressLabel.numberOfLines = 0
         locationAddressLabel.lineBreakMode = NSLineBreakMode.byWordWrapping
         
         submitReviewButton = UIButton()
@@ -107,96 +110,80 @@ class LocationProfileView: UIView, GMSMapViewDelegate {
     }
     
     func constrain() {
-//
-//        scrollView.addSubview(streetView)
-//        streetView.snp.makeConstraints {
-//            $0.top.leading.trailing.equalToSuperview()
-//
-//        }
-        
         addSubview(scrollView)
         scrollView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
         
-//        scrollView.addSubview(streetView)
-//        streetView.snp.makeConstraints {
-//            $0.top.leading.trailing.equalTo(scrollView)
-//            $0.height.equalTo(streetView.snp.width)
-//        }
         scrollView.addSubview(streetView)
         streetView.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.top.equalTo(scrollView.snp.top)
-            $0.width.equalToSuperview()
-            $0.height.equalTo(streetView.snp.width)
-        }
-        
-        
-        scrollView.addSubview(locationProfileImage)
-        locationProfileImage.snp.makeConstraints {
-            $0.leadingMargin.equalToSuperview().offset(10)
-            $0.topMargin.equalTo(scrollView.snp.top).offset(20)
-            $0.width.equalToSuperview().dividedBy(3)
-            $0.height.equalTo(locationProfileImage.snp.width)
-        }
-        
-        addSubview(locationNameLabel)
-        locationNameLabel.snp.makeConstraints {
-            $0.leading.equalTo(locationProfileImage.snp.trailing).offset(5)
-            $0.trailing.equalToSuperview()
-            $0.topMargin.equalTo(scrollView).offset(10)
-            $0.bottom.equalTo(locationProfileImage).dividedBy(3)
-        }
-        /*
-        addSubview(locationAddressLabel)
-        locationAddressLabel.snp.makeConstraints {
-            $0.leading.equalTo(locationProfileImage.snp.trailing).offset(5)
-            $0.trailing.equalToSuperview()
-            $0.top.equalTo(locationNameLabel.snp.bottom).offset(5)
-            $0.height.equalTo(locationNameLabel.snp.height).dividedBy(2)
-        }
-        addSubview(submitReviewButton)
-        submitReviewButton.snp.makeConstraints {
-            $0.leading.equalTo(locationProfileImage.snp.trailing).offset(30)
-            $0.trailing.equalToSuperview().offset(-30)
-            $0.bottom.equalTo(locationProfileImage.snp.bottom)
-        }
-        
-        addSubview(starReviews)
-        starReviews.snp.makeConstraints {
-            $0.leading.equalTo(locationProfileImage.snp.trailing).offset(30)
-            $0.trailing.equalToSuperview().offset(-30)
-            $0.top.equalTo(locationAddressLabel.snp.bottom).offset(5)
-            $0.bottom.equalTo(submitReviewButton.snp.top)
-
-        }
-
-        addSubview(streetView)
-        streetView.snp.makeConstraints {
-            $0.top.equalTo(locationProfileImage.snp.bottom)
-            $0.height.equalToSuperview().dividedBy(2)
-            $0.leading.equalToSuperview()
-            $0.trailing.equalToSuperview()
+            $0.width.equalTo(scrollView.snp.width)
+            $0.height.equalTo(streetView.snp.width).multipliedBy(0.6)
         }
         
         streetView.addSubview(panoView)
         panoView.snp.makeConstraints {
-            $0.edges.equalTo(UIEdgeInsetsMake(20, 20, 20, 20))
+            $0.edges.equalToSuperview()
+        }
+        
+        scrollView.addSubview(locationProfileImage)
+        locationProfileImage.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(10)
+            $0.top.equalTo(streetView.snp.bottom).offset(10)
+            $0.width.equalToSuperview().dividedBy(3)
+            $0.height.equalTo(locationProfileImage.snp.width)
+            scrollView.addSubview(streetView)
+        }
+        
+        scrollView.addSubview(locationNameLabel)
+        locationNameLabel.snp.makeConstraints {
+            $0.leading.equalTo(locationProfileImage.snp.trailing).offset(10)
+            $0.top.equalTo(streetView.snp.bottom).offset(12)
+            $0.height.equalTo(locationProfileImage).dividedBy(2)
+            $0.width.equalToSuperview().multipliedBy(0.66)
         }
         
         
-        addSubview(reviewsView)
+        scrollView.addSubview(locationAddressLabel)
+        locationAddressLabel.snp.makeConstraints {
+            $0.leading.equalTo(locationProfileImage.snp.trailing).offset(10)
+            $0.top.equalTo(locationNameLabel.snp.bottom).offset(10)
+            $0.height.equalTo(locationProfileImage.snp.height).dividedBy(2)
+            $0.width.equalToSuperview().multipliedBy(0.66)
+        }
+        
+        scrollView.addSubview(starReviews)
+        starReviews.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(10)
+            $0.top.equalTo(locationProfileImage.snp.bottom).offset(2)
+            $0.width.equalTo(locationProfileImage.snp.width)
+            $0.height.equalTo(starReviews.frame.height)
+        }
+        
+        
+        scrollView.addSubview(submitReviewButton)
+        submitReviewButton.snp.makeConstraints {
+            $0.centerX.equalTo(locationNameLabel.snp.centerX)
+            $0.top.equalTo(locationAddressLabel.snp.bottom).offset(2)
+            $0.height.lessThanOrEqualTo(60)
+            $0.width.equalTo(locationNameLabel.snp.width).multipliedBy(0.75)
+        }
+        
+        scrollView.addSubview(reviewsView)
         reviewsView.snp.makeConstraints {
-            $0.leading.trailing.bottom.equalToSuperview()
-            $0.top.equalTo(streetView.snp.bottom)
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(starReviews.snp.bottom)
+            $0.width.equalTo(scrollView.snp.width)
+            $0.height.equalTo(locationProfileImage.snp.height).multipliedBy(5)
         }
         
         reviewsView.addSubview(reviewsTableView)
         reviewsTableView.snp.makeConstraints {
-            $0.edges.equalTo(UIEdgeInsetsMake(0, 20, 20, 20))
+            $0.edges.equalToSuperview()
         }
-    */
+        
     }
     
     
